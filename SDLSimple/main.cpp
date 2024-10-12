726f0c06ae964abcc13acdabf7ba53fb5d551d22
yawnka <ys4780@nyu.edu>
2024-10-12 17:38:39 -0400

    Added author comments at the top

--- SDLSimple/main.cpp
+++ SDLSimple/main.cpp
@@ -1,3 +1,12 @@
+/**
+* Author:  Yanka Sikder
+* Assignment: Pong Clone
+* Date due: 2024-10-12, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
 #define GL_SILENCE_DEPRECATION
 #define STB_IMAGE_IMPLEMENTATION
 #define LOG(argument) std::cout << argument << '\n'
